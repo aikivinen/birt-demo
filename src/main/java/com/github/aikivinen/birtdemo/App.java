c21d2d640bf144d171247939cc2d6b5858179c69
Anssi Kivinen <aikivinen@users.noreply.github.com>
2016-12-16 17:43:45 +0200

    Update App.java

--- src/main/java/com/github/aikivinen/birtdemo/App.java
+++ src/main/java/com/github/aikivinen/birtdemo/App.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @EnableAutoConfiguration(exclude=MongoAutoConfiguration.class) // a bug in spring boot?: https://github.com/spring-projects/spring-boot/issues/2196
-@ComponentScan("com.example.birtp")
+@ComponentScan("com.github.aikivinen.birtdemo")
 public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
